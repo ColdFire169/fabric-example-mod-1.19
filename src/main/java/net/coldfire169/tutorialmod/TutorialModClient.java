fdbda0db4fbb4da81cb0c4e4536b9a2d9520cc1c
Chase Torres <coldfire169@gmail.com>
2022-06-20 19:00:11 -0600

    Initial commit

---
+++ src/main/java/net/coldfire169/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.coldfire169.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
